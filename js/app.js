c92cbf790f95acab090410e19efa99bf81d8ddca
diptanshumahish <diptanshumahish2016@gmail.com>
2022-09-12 20:56:01 +0530

    auto play on the news cards

--- js/app.js
+++ js/app.js
@@ -19,21 +19,26 @@ const lapToggler =document.getElementById('themeTogglerBtnLap');
 const theme = window.localStorage.getItem("theme");
 if (theme == "dark") {
     document.body.classList.add("dark");
+      
 }
 toggler.addEventListener('click', function () {
     document.body.classList.toggle("dark");
     if (theme == "dark") {
         window.localStorage.setItem("theme", "light");
+     
     } else {
-        window.localStorage.setItem("theme", "dark")
+        window.localStorage.setItem("theme", "dark");
+        
     }
 })
 lapToggler.addEventListener('click', function () {
     document.body.classList.toggle("dark");
     if (theme == "dark") {
         window.localStorage.setItem("theme", "light");
+       
     } else {
-        window.localStorage.setItem("theme", "dark")
+        window.localStorage.setItem("theme", "dark");
+        
     }
 })
 
